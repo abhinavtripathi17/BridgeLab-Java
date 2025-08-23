ef9ea0331fd9c50992f620d04b9a7894cb261b63
abhinavtripathi17 <abhinavtripathi1708@gmail.com>
2025-08-23 22:01:28 +0530

    Control Flows Assignment done

---
+++ Control Flow/Level1/quest11.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class quest11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double total = 0.0;
+        
+        while (true) {
+            System.out.print("Enter a number: ");
+            double number = input.nextDouble();
+            
+            if (number <= 0) {
+                break;
+            }
+            
+            total += number;
+        }
+        
+        System.out.println("Total sum: " + total);
+        input.close();
+    }
+}
